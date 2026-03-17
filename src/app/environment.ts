5c78403406703824bdf6ef5be29e4f62473dd7e8
Bg <bhavyagodhaniya2004@gmail.com>
2026-03-17 22:28:44 +0530

    modify deployment url of online compiler

--- src/app/environment.ts
+++ src/app/environment.ts
@@ -4,5 +4,5 @@ export const environment = {
   //exaxm portal server
   apiUrl: production ? 'https://proxy-0xaq.onrender.com/api2' : "http://localhost:8081",
   //code runner backend
-  apiUrl2 : production?"https://compilerbackend2.onrender.com":"http://localhost:8083",
+  apiUrl2 : production?"https://proxy-0xaq.onrender.com/api3":"http://localhost:8083",
 };
