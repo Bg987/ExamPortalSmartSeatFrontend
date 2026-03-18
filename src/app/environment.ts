98deea341383d90f95cb48a9a210ac6e023ab69e
Bg <bhavyagodhaniya2004@gmail.com>
2026-03-18 11:01:46 +0530

    ....

--- src/app/environment.ts
+++ src/app/environment.ts
@@ -2,7 +2,7 @@
 let production = true;
 export const environment = {
   //exaxm portal server
-  apiUrl: production ? 'https://proxy-0xaq.onrender.com/api2' : "http://localhost:8081",
+  apiUrl: production ? 'https://examportalsmartseatbackend.onrender.com/' : "http://localhost:8081",
   //code runner backend
-  apiUrl2 : production?"https://proxy-0xaq.onrender.com/api3":"http://localhost:8083",
+  apiUrl2 : production?"https://compilerbackend2.onrender.com/":"http://localhost:8083",
 };
